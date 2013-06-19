2d8665d827cc59ad95164bae726f780f15040f86
Sebastian Kovacs <sebastian.kovacs@barandisolutions.ro>
2013-06-19 11:26:54 +0300

    initial commit

---
+++ app/services/data.js
@@ -0,0 +1,25 @@
+/*
+ * Data service
+ */
+angular.module('app').factory('data', [
+	'$rootScope',
+	'$filter',
+	function($rootScope, $filter) {
+	
+		
+		
+		// methods
+		var GetSomething = function(params) {
+				
+			
+		
+		};
+		
+		
+		
+		return {
+			GetSomething: GetSomething
+		}
+		
+	}
+]);
